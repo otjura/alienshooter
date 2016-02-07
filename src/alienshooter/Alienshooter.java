d7bd83a725142137e9b5620b15396981096467a2
otjura <ojrajala@gmail.com>
2016-02-07 21:08:21 +0200

    initial commit

---
+++ src/alienshooter/Alienshooter.java
@@ -0,0 +1,34 @@
+package alienshooter;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Alienshooter extends JFrame {
+    
+    public Alienshooter() {     
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());
+        
+        setResizable(false);
+        pack();
+        
+        setTitle("Alienshooter");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Alienshooter ex = new Alienshooter();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
