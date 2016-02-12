c68961b30d338183699014d59d68b23b74d49a70
otjura <ojrajala@gmail.com>
2016-02-12 14:03:19 +0200

    new classes

---
+++ src/alienshooter/Laser.java
@@ -0,0 +1,5 @@
+package alienshooter;
+
+public class Laser extends Weapon {
+
+}
---
+++ src/alienshooter/Startscreen.java
@@ -0,0 +1,11 @@
+package alienshooter;
+
+import javax.swing.JPanel;
+import java.awt.Image;
+
+public class Startscreen {
+    
+    public Startscreen() {
+        
+    }
+}
