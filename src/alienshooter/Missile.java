561fba1922b4db88bbafe2958474a04e6df8a84f
otjura <ojrajala@gmail.com>
2016-02-15 22:49:45 +0200

    sprite changes according to weapon

--- src/alienshooter/Missile.java
+++ src/alienshooter/Missile.java
@@ -4,10 +4,19 @@ public class Missile extends Sprite {
     private final int BOARD_WIDTH = 790;
     private final int BOARD_HEIGHT = 590;
     private final int MISSILE_SPEED = 3;
+    private Weapontype weapontype;
+    
+    public enum Weapontype { LASER, BLASTER, ZINGER; }
 
     public Missile(int x, int y) {
         super(x, y);
-        loadImage("images/missile.png");
+        switch(weapontype) {
+            case LASER: loadImage("images/laser.png"); break;
+            case BLASTER: loadImage("images/blaster.png"); break;
+            case ZINGER: loadImage("images/zinger.png"); break;
+            default: loadImage("images/missile.png"); break;
+        }
+        
         getImageDimensions();        
     }
 
