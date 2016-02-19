dcbe9e358703149711aa285cddaf2e977ecc4b33
otjura <ojrajala@gmail.com>
2016-02-19 15:24:36 +0200

    Craft has weapon, not Player

--- src/alienshooter/Craft.java
+++ src/alienshooter/Craft.java
@@ -9,11 +9,13 @@ public class Craft extends Sprite {
     protected int hitpoints;
     private final int SPEED = 2;
     private ArrayList<Missile> missiles;
+    private Weapon weapon;
 
     public Craft(int x, int y) {
         super(x, y);
         hitpoints = 100;
         missiles = new ArrayList<>();
+        weapon = new Weapon(Weapontype.LASER);
         loadImage("images/craft.png");
         getImageDimensions();
     }
@@ -21,8 +23,8 @@ public class Craft extends Sprite {
     public void move() {
         x += dx;
         y += dy;
-        if (x < 1) { x = 1; }
-        if (y < 1) { y = 1; }
+        if (x < 1) x = 1;
+        if (y < 1) y = 1;
     }
 
     public ArrayList getMissiles() { return missiles; }
@@ -30,30 +32,37 @@ public class Craft extends Sprite {
 
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
-        if (key == KeyEvent.VK_Z) { fire(); }
-        if (key == KeyEvent.VK_LEFT) { dx = -SPEED; }
-        if (key == KeyEvent.VK_RIGHT) { dx = SPEED; }
-        if (key == KeyEvent.VK_UP) { dy = -SPEED; }
-        if (key == KeyEvent.VK_DOWN) { dy = SPEED; }
-        if (key == KeyEvent.VK_ENTER) { pause(); }
+        if (key == KeyEvent.VK_Z)     fire();
+        if (key == KeyEvent.VK_X)     bomb();
+        if (key == KeyEvent.VK_C)     changeWeapon();
+        if (key == KeyEvent.VK_LEFT)  dx = -SPEED;
+        if (key == KeyEvent.VK_RIGHT) dx = SPEED;
+        if (key == KeyEvent.VK_UP)    dy = -SPEED;
+        if (key == KeyEvent.VK_DOWN)  dy = SPEED;
+        if (key == KeyEvent.VK_ENTER) pause();
     }
     
     public void keyReleased(KeyEvent e) {
         int key = e.getKeyCode();
-        if (key == KeyEvent.VK_LEFT) { dx = 0; }
-        if (key == KeyEvent.VK_RIGHT) { dx = 0; }
-        if (key == KeyEvent.VK_UP) { dy = 0; }
-        if (key == KeyEvent.VK_DOWN) { dy = 0; }
+        if (key == KeyEvent.VK_LEFT)  dx = 0;
+        if (key == KeyEvent.VK_RIGHT) dx = 0;
+        if (key == KeyEvent.VK_UP)    dy = 0;
+        if (key == KeyEvent.VK_DOWN)  dy = 0;
     }
 
     public void fire() {
-        //do {
         missiles.add(new Missile(x + width, y + height / 2));
-        //Thread.sleep(200);
-        //} while(true);
-    } 
+    }
+    
+    public void bomb() {
+        fire();
+    }
+    
+    public void changeWeapon() {
+        fire();         
+    }
     
     protected void pause() { 
-        
+        fire();
     }
 }
--- src/alienshooter/Player.java
+++ src/alienshooter/Player.java
@@ -2,17 +2,13 @@ package alienshooter;
 
 public class Player {
     private int score;
-    private Weapon weapon;
     
     public Player() {
         this.score = 0;
-        this.weapon = new Laser();
     }
     
     public int getScore() { return score; }
-    public Weapon getWeapon() { return weapon; }
     
-    public void setWeapon(Weapon weapon) { this.weapon = weapon; }
     public void setScore(int score) { this.score = score; } 
     
     public void increaseScore(int increase) { score += increase; } 
