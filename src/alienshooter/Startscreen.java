551e2232e5d104b63aff191677bd45855cda3c7e
otjura <ojrajala@gmail.com>
2016-02-25 23:55:14 +0200

    start work on startscreen

--- src/alienshooter/Startscreen.java
+++ src/alienshooter/Startscreen.java
@@ -1,11 +1,30 @@
 package alienshooter;
 
-import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import javax.swing.JPanel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class Startscreen {
+public class Startscreen extends JPanel implements ActionListener {
     
     public Startscreen() {
         
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("TODO");
     }
 }
